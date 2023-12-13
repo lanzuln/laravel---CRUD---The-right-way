5249c4a7a863ad57734a38d92bf5000b1408d1c7
lanzuln <lanzu8006@gmail.com>
2023-12-13 23:54:08 +0600

    add field on offer migration, and add const instead of static value

---
+++ app/Constants/Status.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Constants;
+class Status{
+    public const DRAFT = 'draft';
+    public const PUBLISHED = 'published';
+}
--- database/migrations/2023_12_13_173006_create_offers_table.php
+++ database/migrations/2023_12_13_173006_create_offers_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Constants\Status;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->float('price');
+            $table->string('status')->default(Status::DRAFT);
+            $table->foreignId('author_id')->constrained('users');
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
