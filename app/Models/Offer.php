cc3b38e10d021fd026424704def030fe7f457258
lanzuln <lanzu8006@gmail.com>
2023-12-14 18:48:25 +0600

    model ready

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+    protected $fillable=[
+    'title',
+    'description',
+    'price',
+    'status',
+    'author_id',
+    ];
 }
